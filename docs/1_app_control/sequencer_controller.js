2b084e063fb87a95694af0343aefd8219d6507ea
vird <virdvip@gmail.com>
2021-03-27 16:04:37 +0000

    build

--- docs/1_app_control/sequencer_controller.js
+++ docs/1_app_control/sequencer_controller.js
@@ -16,7 +16,19 @@
 
     Sequencer_controller.prototype.play_state = false;
 
-    Sequencer_controller.prototype.mode_hide_future = true;
+    Sequencer_controller.prototype.mode_hide_future = false;
+
+    Sequencer_controller.prototype.display_block = true;
+
+    Sequencer_controller.prototype.display_tx_pow = true;
+
+    Sequencer_controller.prototype.display_tx_pow_src_lines = true;
+
+    Sequencer_controller.prototype.speed_scale = 1;
+
+    Sequencer_controller.prototype.show_help = false;
+
+    Sequencer_controller.prototype.speed_step = 0.1;
 
     Sequencer_controller.prototype.canvas_controller = function(canvas_hash) {
       var $canvas_panel_fg;
@@ -29,7 +41,7 @@
     };
 
     Sequencer_controller.prototype.init = function() {
-      var fn;
+      var fn, rel_speed, rel_ts;
       global_mouse_up.on("mouse_up", this.global_mouse_up_handler = (function(_this) {
         return function(e) {
           return _this.mouse_up(e);
@@ -49,8 +61,75 @@
           return _this.refresh();
         };
       })(this);
-      return this.scheme.active_scheme.register("home", fn, {
-        description: "Go to start of video"
+      this.scheme.active_scheme.register("home", fn, {
+        description: "Go to start"
+      });
+      fn = (function(_this) {
+        return function() {
+          _this.show_help = !_this.show_help;
+          return _this.com.force_update();
+        };
+      })(this);
+      this.scheme.active_scheme.register("f1", fn, {
+        description: "toggle help"
+      });
+      rel_speed = (function(_this) {
+        return function(step) {
+          var new_speed;
+          new_speed = +(_this.speed_scale + step).toFixed(2);
+          if (new_speed > 0) {
+            return _this.speed_scale_set(new_speed);
+          }
+        };
+      })(this);
+      this.scheme.active_scheme.register("num_plus", ((function(_this) {
+        return function() {
+          return rel_speed(+_this.speed_step);
+        };
+      })(this)), {
+        description: "increase speed"
+      });
+      this.scheme.active_scheme.register("num_minus", ((function(_this) {
+        return function() {
+          return rel_speed(-_this.speed_step);
+        };
+      })(this)), {
+        description: "decrease speed"
+      });
+      rel_ts = (function(_this) {
+        return function(diff) {
+          debugger;
+          _this.ts_set(_this.model.ts + diff);
+          return _this.refresh();
+        };
+      })(this);
+      this.scheme.active_scheme.register("left", ((function(_this) {
+        return function() {
+          return rel_ts(-1000);
+        };
+      })(this)), {
+        description: "-1 sec"
+      });
+      this.scheme.active_scheme.register("right", ((function(_this) {
+        return function() {
+          return rel_ts(+1000);
+        };
+      })(this)), {
+        description: "+1 sec"
+      });
+      this.scheme.active_scheme.register("comma", ((function(_this) {
+        return function() {
+          return rel_ts(-100);
+        };
+      })(this)), {
+        description: "-0.1 sec"
+      });
+      return this.scheme.active_scheme.register("dot", ((function(_this) {
+        return function() {
+          return rel_ts(+100);
+        };
+      })(this)), {
+        description: "+0.1 sec"
       });
     };
 
@@ -66,7 +145,7 @@
     Sequencer_controller.prototype.size_x = 1;
 
     Sequencer_controller.prototype.redraw_panel_fg = function() {
-      var bar_pad, canvas, ctx, delimiter_y, display_size_x, ed_ts, event, filter_a_ts, filter_b_ts, font_size_x, font_size_y, idx, left_panel_size_x, left_panel_text_x, node, node_icon_count, node_icon_offset_top, node_icon_pad, node_icon_pad_left, node_icon_size, node_state, node_state_list, pad, pattern_bar_size_y, pow_blink_duration, pow_type_color, pow_type_color_disabled, ruler_pad, size_x, size_y, text_offset_top, ts, type_idx, x, y, _i, _j, _k, _l, _len, _len1, _len2, _len3, _len4, _len5, _m, _n, _o, _p, _ref, _ref1, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7;
+      var bar_pad, block_color, block_drop_color, block_drop_ts, block_size_x, canvas, ctx, delimiter_ts, delimiter_y, display_size_x, ed_ts, event, event_idx, filter_a_ts, filter_b_ts, font_size_x, font_size_y, future_event, future_event_idx, idx, left_panel_size_x, left_panel_text_x, node, node_bar_size_y, node_icon2_offset_left, node_icon2_offset_top, node_icon2_pad, node_icon2_size, node_icon2_size_2, node_icon_count, node_icon_offset_top, node_icon_pad, node_icon_pad_left, node_icon_size, node_idx, node_state, node_state_list, pad, pow_blink_duration, pow_type_color, pow_type_color_disabled, round_id, ruler_pad, size_x, size_y, t, text_offset_top, ts, ts_max, tx_pow, type_idx, x, x_a, x_b, y, y_a, y_b, _i, _j, _k, _l, _len, _len1, _len2, _len3, _len4, _len5, _len6, _len7, _len8, _len9, _m, _n, _o, _p, _q, _r, _ref, _ref1, _ref10, _ref11, _ref12, _ref13, _ref14, _ref2, _ref3, _ref4, _ref5, _ref6, _ref7, _ref8, _ref9, _s, _t, _u;
       if (!this.has_redraw_changes_panel_fg) {
         return;
       }
@@ -84,36 +163,45 @@
       if (!this.model) {
         return;
       }
-      ts = this.model.ts;
+      _ref = this.model, ts = _ref.ts, ts_max = _ref.ts_max;
+      block_color = "#AAFFAA";
+      block_drop_color = "#AAAAAA";
       pow_type_color = ["#00FFFF", "#0094FF", "#0026FF", "#B200FF"];
-      pattern_bar_size_y = 20;
+      node_bar_size_y = 20;
       pad = 4;
       node_icon_count = pow_type_color.length;
       node_icon_size = 16;
       node_icon_pad = 2;
       node_icon_pad_left = 4;
       node_icon_offset_top = 3;
+      node_icon2_size = 10;
+      node_icon2_pad = 4;
+      node_icon2_offset_left = 3;
+      node_icon2_offset_top = 3;
       text_offset_top = -3;
       bar_pad = 8;
       ruler_pad = 20;
       this.left_panel_size_x = left_panel_size_x = 300;
-      font_size_y = pattern_bar_size_y - pad;
+      node_icon2_size_2 = node_icon2_size / 2;
+      block_size_x = node_icon_count * (node_icon2_size + node_icon2_pad) + 3;
+      font_size_y = node_bar_size_y - pad;
       font_size_x = font_size_y / 1.8;
       left_panel_text_x = (node_icon_count + 1) * (node_icon_size + node_icon_pad) + node_icon_pad_left;
+      display_size_x = size_x - left_panel_size_x;
       pow_type_color_disabled = "#AAAAAA";
       node_state_list = [];
       pow_blink_duration = 1000;
       filter_a_ts = ts - 1000;
       filter_b_ts = ts;
-      _ref = this.model.node_list;
-      for (idx = _i = 0, _len = _ref.length; _i < _len; idx = ++_i) {
-        node = _ref[idx];
+      _ref1 = this.model.node_list;
+      for (idx = _i = 0, _len = _ref1.length; _i < _len; idx = ++_i) {
+        node = _ref1[idx];
         node_state_list.push(node_state = {
           pow_icon_opacity: [0, 0, 0, 0]
         });
-        _ref1 = node.event_list;
-        for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
-          event = _ref1[_j];
+        _ref2 = node.event_list;
+        for (_j = 0, _len1 = _ref2.length; _j < _len1; _j++) {
+          event = _ref2[_j];
           if (event.ts < filter_a_ts) {
             continue;
           }
@@ -132,18 +220,18 @@
       ctx.strokeStyle = "#000";
       ctx.fillRect(0.5 + 1, 0.5 + 1, left_panel_size_x - 1, size_y - 2);
       ctx.strokeRect(0.5 + 0, 0.5 + 0, left_panel_size_x - 2, size_y - 1);
-      _ref2 = this.model.node_list;
-      for (idx = _k = 0, _len2 = _ref2.length; _k < _len2; idx = ++_k) {
-        node = _ref2[idx];
+      _ref3 = this.model.node_list;
+      for (idx = _k = 0, _len2 = _ref3.length; _k < _len2; idx = ++_k) {
+        node = _ref3[idx];
         node_state = node_state_list[idx];
-        y = 0.5 + idx * pattern_bar_size_y + node_icon_offset_top;
+        y = 0.5 + idx * node_bar_size_y + node_icon_offset_top;
         ctx.globalAlpha = 1;
-        for (type_idx = _l = 0, _ref3 = pow_type_color.length; 0 <= _ref3 ? _l < _ref3 : _l > _ref3; type_idx = 0 <= _ref3 ? ++_l : --_l) {
+        for (type_idx = _l = 0, _ref4 = pow_type_color.length; 0 <= _ref4 ? _l < _ref4 : _l > _ref4; type_idx = 0 <= _ref4 ? ++_l : --_l) {
           ctx.fillStyle = pow_type_color_disabled;
           x = type_idx * (node_icon_size + node_icon_pad) + node_icon_pad_left;
           ctx.fillRect(x, y, node_icon_size, node_icon_size);
         }
-        for (type_idx = _m = 0, _ref4 = pow_type_color.length; 0 <= _ref4 ? _m < _ref4 : _m > _ref4; type_idx = 0 <= _ref4 ? ++_m : --_m) {
+        for (type_idx = _m = 0, _ref5 = pow_type_color.length; 0 <= _ref5 ? _m < _ref5 : _m > _ref5; type_idx = 0 <= _ref5 ? ++_m : --_m) {
           ctx.globalAlpha = node_state.pow_icon_opacity[type_idx];
           ctx.fillStyle = pow_type_color[type_idx];
           x = type_idx * (node_icon_size + node_icon_pad) + node_icon_pad_left;
@@ -158,10 +246,10 @@
       ctx.strokeStyle = "rgba(0,0,0,0.3)";
       ctx.font = "" + font_size_y + "px monospace";
       ctx.globalAlpha = 1;
-      _ref5 = this.model.node_list;
-      for (idx = _n = 0, _len3 = _ref5.length; _n < _len3; idx = ++_n) {
-        node = _ref5[idx];
-        y = 0.5 + -1 + (idx + 1) * pattern_bar_size_y + text_offset_top;
+      _ref6 = this.model.node_list;
+      for (idx = _n = 0, _len3 = _ref6.length; _n < _len3; idx = ++_n) {
+        node = _ref6[idx];
+        y = 0.5 + -1 + (idx + 1) * node_bar_size_y + text_offset_top;
         ctx.fillText(node.title, left_panel_text_x, y);
         delimiter_y = 4 + 0.5 + Math.round(y);
         ctx.beginPath();
@@ -170,34 +258,124 @@
         ctx.stroke();
         ctx.closePath();
       }
-      _ref6 = this.model.node_list;
-      for (idx = _o = 0, _len4 = _ref6.length; _o < _len4; idx = ++_o) {
-        node = _ref6[idx];
-        _ref7 = node.event_list;
-        for (_p = 0, _len5 = _ref7.length; _p < _len5; _p++) {
-          event = _ref7[_p];
-          if (this.mode_hide_future) {
-            if (event.ts > filter_b_ts) {
-              break;
+      if (this.display_tx_pow) {
+        _ref7 = this.model.node_list;
+        for (idx = _o = 0, _len4 = _ref7.length; _o < _len4; idx = ++_o) {
+          node = _ref7[idx];
+          y = 0.5 + idx * node_bar_size_y + node_icon_offset_top;
+          _ref8 = node.event_list;
+          for (_p = 0, _len5 = _ref8.length; _p < _len5; _p++) {
+            event = _ref8[_p];
+            if (this.mode_hide_future) {
+              if (event.ts > filter_b_ts) {
+                break;
+              }
             }
-          }
-          if (event.type !== "tx_pow_mine") {
-            continue;
+            if (event.type !== "tx_pow_mine") {
+              continue;
+            }
+            t = event.ts / ts_max;
+            x = left_panel_size_x + display_size_x * t;
+            ctx.fillStyle = pow_type_color[event.tx_pow_type];
+            ctx.fillRect(x, y, node_icon_size, node_icon_size);
           }
         }
       }
-      display_size_x = size_x - left_panel_size_x;
-      x = left_panel_size_x + (ts / this.model.ts_max) * display_size_x;
+      if (this.display_block) {
+        _ref9 = this.model.node_list;
+        for (node_idx = _q = 0, _len6 = _ref9.length; _q < _len6; node_idx = ++_q) {
+          node = _ref9[node_idx];
+          y = 0.5 + node_idx * node_bar_size_y + node_icon_offset_top;
+          _ref10 = node.event_list;
+          for (event_idx = _r = 0, _len7 = _ref10.length; _r < _len7; event_idx = ++_r) {
+            event = _ref10[event_idx];
+            if (this.mode_hide_future) {
+              if (event.ts > filter_b_ts) {
+                break;
+              }
+            }
+            if (event.type !== "block") {
+              continue;
+            }
+            block_drop_ts = null;
+            for (future_event_idx = _s = _ref11 = event_idx + 1, _ref12 = node.event_list.length; _s < _ref12; future_event_idx = _s += 1) {
+              future_event = node.event_list[future_event_idx];
+              if (future_event.type === "block") {
+                break;
+              }
+              if (future_event.type === "block_drop") {
+                if (future_event.ts < filter_b_ts) {
+                  block_drop_ts = future_event.ts;
+                }
+              }
+            }
+            t = event.ts / ts_max;
+            x = 0.5 + left_panel_size_x + display_size_x * t;
+            ctx.fillStyle = block_drop_ts != null ? block_drop_color : block_color;
+            ctx.strokeStyle = "#000";
+            ctx.fillRect(x, y, block_size_x, node_icon_size);
+            ctx.strokeRect(x, y, block_size_x, node_icon_size);
+            x += node_icon2_offset_left;
+            y += node_icon2_offset_top;
+            _ref13 = event.tx_pow_list;
+            for (_t = 0, _len8 = _ref13.length; _t < _len8; _t++) {
+              tx_pow = _ref13[_t];
+              if (this.display_tx_pow_src_lines && tx_pow.source_idx !== node_idx) {
+                ctx.beginPath();
+                ctx.moveTo(x + node_icon2_size_2, y + node_icon2_size_2);
+                ctx.lineTo(x + node_icon2_size_2, y + (node_bar_size_y * (tx_pow.source_idx - node_idx)));
+                ctx.stroke();
+                ctx.closePath();
+              }
+              ctx.fillStyle = pow_type_color[tx_pow.tx_pow_type];
+              ctx.fillRect(x, y, node_icon2_size, node_icon2_size);
+              ctx.strokeRect(x, y, node_icon2_size, node_icon2_size);
+              x += node_icon2_size + node_icon2_pad;
+            }
+            if (block_drop_ts) {
+              t = block_drop_ts / ts_max;
+              x = 0.5 + left_panel_size_x + display_size_x * t;
+              ctx.beginPath();
+              x_a = x;
+              y_a = y;
+              x_b = x + node_icon2_size;
+              y_b = y + node_icon2_size;
+              ctx.moveTo(x_a, y_a);
+              ctx.lineTo(x_b, y_b);
+              ctx.moveTo(x_a, y_b);
+              ctx.lineTo(x_b, y_a);
+              ctx.stroke();
+              ctx.closePath();
+            }
+          }
+        }
+      }
+      round_id = 0;
+      _ref14 = this.model.round_delimiter_ts_list;
+      for (_u = 0, _len9 = _ref14.length; _u < _len9; _u++) {
+        delimiter_ts = _ref14[_u];
+        if (delimiter_ts < ts) {
+          round_id++;
+        }
+        x = left_panel_size_x + (delimiter_ts / ts_max) * display_size_x;
+        ctx.strokeStyle = "#F00";
+        ctx.beginPath();
+        ctx.moveTo(x, 0.5 + 0);
+        ctx.lineTo(x, 0.5 - 1 + size_y);
+        ctx.stroke();
+      }
+      x = left_panel_size_x + (ts / ts_max) * display_size_x;
       x = 0.5 + Math.round(x);
       ctx.strokeStyle = "#000";
+      ctx.fillStyle = "#000";
       ctx.beginPath();
       ctx.moveTo(x, 0.5 + 0);
       ctx.lineTo(x, 0.5 - 1 + size_y);
       ctx.stroke();
-      x = size_x - pad;
-      y = font_size_y;
+      x = 0.5 + size_x - pad;
+      y = 0.5 + font_size_y;
       ctx.textAlign = "right";
-      ctx.fillText("" + ((ts / 1000).toFixed(2)), x, y);
+      ctx.fillText("round \#" + (round_id + 1) + " " + ((ts / 1000).toFixed(2).rjust(6)), x, y);
       ctx.textAlign = "left";
     };
 
@@ -247,6 +425,13 @@
 
     Sequencer_controller.prototype.start_ts = 0;
 
+    Sequencer_controller.prototype.ts_set = function(ts) {
+      ts = Math.max(0, ts);
+      ts = Math.min(this.model.ts_max, ts);
+      this.model.ts = ts;
+      return this.start_ts = Date.now() - this.model.ts / this.speed_scale;
+    };
+
     Sequencer_controller.prototype.play = function() {
       if (this._animation_interval) {
         clearInterval(this._animation_interval);
@@ -256,12 +441,12 @@
       if (this.model.ts === this.model.ts_max) {
         this.start_ts = Date.now();
       } else {
-        this.start_ts = Date.now() - this.model.ts;
+        this.ts_set(this.model.ts);
       }
       return this._animation_interval = setInterval((function(_this) {
         return function() {
           var ts;
-          ts = Date.now() - _this.start_ts;
+          ts = (Date.now() - _this.start_ts) * _this.speed_scale;
           if (ts >= _this.model.ts_max) {
             ts = _this.model.ts_max;
             _this.stop();
@@ -295,8 +480,14 @@
       t = Math.max(0, t);
       t = Math.min(1, t);
       ts = t * this.model.ts_max;
-      this.model.ts = ts;
-      return this.play();
+      this.ts_set(ts);
+      return this.refresh();
+    };
+
+    Sequencer_controller.prototype.speed_scale_set = function(speed_scale) {
+      this.speed_scale = speed_scale;
+      this.ts_set(this.model.ts);
+      return this.com.force_update();
     };
 
     return Sequencer_controller;
--- docs/1_app_control/sequencer_panel.com.js
+++ docs/1_app_control/sequencer_panel.com.js
@@ -10,6 +10,7 @@
       return this.controller.model = {
         ts: 0,
         ts_max: 5000,
+        round_delimiter_ts_list: [2000],
         node_list: [
           {
             title: "Node 1",
@@ -71,8 +72,8 @@
                 ts: 100,
                 tx_pow_list: []
               }, {
-                type: "block_shadow",
-                ts: 300,
+                type: "block_drop",
+                ts: 400,
                 tx_pow_list: []
               }
             ]
@@ -93,16 +94,96 @@
           return tbody(function() {
             tr(function() {
               return td(function() {
+                return div({
+                  style: {
+                    display: _this.controller.show_help ? "" : "none"
+                  }
+                }, function() {
+                  var _ref;
+                  if ((_ref = _this.controller.scheme) != null ? _ref.active_scheme : void 0) {
+                    return Keyboard_help_multi({
+                      scheme: _this.controller.scheme.active_scheme
+                    });
+                  }
+                });
+              });
+            });
+            tr(function() {
+              td({
+                style: {
+                  width: 250
+                }
+              }, function() {
                 return Button({
-                  label: _this.controller.play_state ? "stop" : "play",
+                  label: _this.controller.play_state ? "Stop" : "Play",
                   on_click: function() {
                     return _this.controller.toggle_play();
                   }
                 });
               });
+              return td({
+                style: {
+                  color: "#777"
+                }
+              }, "If you focus timeline you can use some hotkeys. Focus and press F1 for help");
+            });
+            tr(function() {
+              td("Speed");
+              return td(Number_input({
+                value: _this.controller.speed_scale,
+                on_change: function(speed_scale) {
+                  return _this.controller.speed_scale_set(speed_scale);
+                }
+              }));
+            });
+            tr(function() {
+              td("Hide future");
+              return td(Checkbox({
+                value: _this.controller.mode_hide_future,
+                on_change: function(mode_hide_future) {
+                  _this.controller.mode_hide_future = mode_hide_future;
+                  _this.controller.refresh();
+                  return _this.force_update();
+                }
+              }));
+            });
+            tr(function() {
+              td("Display tx_pow on timeline");
+              return td(Checkbox({
+                value: _this.controller.display_tx_pow,
+                on_change: function(display_tx_pow) {
+                  _this.controller.display_tx_pow = display_tx_pow;
+                  _this.controller.refresh();
+                  return _this.force_update();
+                }
+              }));
+            });
+            tr(function() {
+              td("Display block");
+              return td(Checkbox({
+                value: _this.controller.display_block,
+                on_change: function(display_block) {
+                  _this.controller.display_block = display_block;
+                  _this.controller.refresh();
+                  return _this.force_update();
+                }
+              }));
+            });
+            tr(function() {
+              td("Display block tx_pow усики");
+              return td(Checkbox({
+                value: _this.controller.display_tx_pow_src_lines,
+                on_change: function(display_tx_pow_src_lines) {
+                  _this.controller.display_tx_pow_src_lines = display_tx_pow_src_lines;
+                  _this.controller.refresh();
+                  return _this.force_update();
+                }
+              }));
             });
             return tr(function() {
-              return td(function() {
+              return td({
+                colSpan: 2
+              }, function() {
                 return Canvas_multi({
                   width: size_x,
                   height: size_y,
